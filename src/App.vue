3adc1b61dd7245f2f7b612219a8a91f9da363579
Mohaimen <mohaimen707@gmail.com>
2019-06-23 12:44:26 +0600

    Initial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Vue Cli</h1>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
